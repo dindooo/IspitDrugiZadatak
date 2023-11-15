e19134b4ffaebfbbb8326f99b2cfadb223568fbb
dinooo <145918377+dindooo@users.noreply.github.com>
2023-11-15 09:26:55 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IspitCetvrti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite prvu rijec");
+            string prvi = Console.ReadLine();
+            Console.WriteLine("Upisite drugu rijec");
+            string drugi = Console.ReadLine();
+            char[] abeceda = prvi.ToCharArray();
+            Array.Sort(abeceda);
+            Console.WriteLine(abeceda);
+            char[] abeceda2 = drugi.ToCharArray();
+            Array.Sort(abeceda2);
+            Console.WriteLine(abeceda2);
+
+            Console.ReadKey();
+       
+
+        }
+    }
+}
